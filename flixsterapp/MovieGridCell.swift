71a8cb5806d71b79aa14785ea7277a50672d3961
Mac561 <maccooper561@gmail.com>
2020-02-09 18:52:44 -0800

    this is the final commit for the flixsterapp

---
+++ flixsterapp/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixsterapp
+//
+//  Created by Mac Cooper on 2/9/20.
+//  Copyright © 2020 Mac Cooper. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
